bd6862d07edb1041b609e6b0c060fb3920641f02
CG.Lee <contact@chulgil.me>
2020-10-02 16:44:00 +0900

    polishing

--- shop-rest-api-with-spring/src/test/java/me/chulgil/shop/controller/ProductsControllerTest.java
+++ shop-rest-api-with-spring/src/test/java/me/chulgil/shop/controller/ProductsControllerTest.java
@@ -3,6 +3,7 @@ package me.chulgil.shop.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -15,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -62,7 +64,9 @@ public class ProductsControllerTest {
 			)
 			.andDo(print())
 			.andExpect(status().isCreated())
-			.andExpect(jsonPath("id").exists());
+			.andExpect(jsonPath("id").exists())
+			.andExpect(header().exists(HttpHeaders.LOCATION))
+			.andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE));
 	}
 
 }
